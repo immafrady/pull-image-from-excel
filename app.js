a93d050b823674f5a1fe36e46a104b38be67647b
Frady@Win <immafrady@gmail.com>
2019-03-21 23:24:35 +0800

    基本完成

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const fs = require('fs')
+const officegen = require('officegen')
+const CommandLineOutput = require('./lib/CommandLineOutput/index')
+const ReadExcel = require('./lib/ReadExcel/index')
+const WriteWord = require('./lib/WriteWord/index')
+const SaveImage = require('./lib/SaveImage/index')
+
+const outDir = path.resolve(process.cwd(), 'output')
+
+async function main() {
+    const filename = await CommandLineOutput.ask('文件名为？(默认：导入.xlsx)')
+    const excelJson = await ReadExcel.readExcel(filename)
+    const myDoc = await WriteWord(excelJson)
+
+    const out = fs.createWriteStream(path.join(outDir, '输出.docx'))
+    myDoc.generate(out)
+    out.on('close', () => {
+        console.log('Mission Completed!')
+    })
+}
+
+main().catch(e => console.log(e))
