ba589c3e221858644a5323907f1c579400d7e58a
Frady@Win <immafrady@gmail.com>
2019-03-22 01:25:09 +0800

    加入另一个方法！

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ const CommandLineOutput = require('./lib/CommandLineOutput/index')
 const ReadExcel = require('./lib/ReadExcel/index')
 const WriteWord = require('./lib/WriteWord/index')
 const SaveImage = require('./lib/SaveImage/index')
+const DirectDownload = require('./lib/DirectDownload/index')
 
 const outDir = path.resolve(process.cwd(), 'output')
 
 async function main() {
     const filename = await CommandLineOutput.ask('文件名为？(默认：导入.xlsx)')
     const excelJson = await ReadExcel.readExcel(filename)
-    const myDoc = await WriteWord(excelJson)
-
-    const out = fs.createWriteStream(path.join(outDir, '输出.docx'))
-    myDoc.generate(out)
-    out.on('close', () => {
-        console.log('Mission Completed!')
-    })
+    // ------------------------ 导出到文件夹
+    DirectDownload(excelJson, outDir)
+    // ------------------------ 导出到word
+    // const myDoc = await WriteWord(excelJson, outDir)
+    //
+    // const out = fs.createWriteStream(path.join(outDir, '输出.docx'))
+    // myDoc.generate(out)
+    // out.on('close', () => {
+    //     console.log('Mission Completed!')
+    // })
 }
 
 main().catch(e => console.log(e))
