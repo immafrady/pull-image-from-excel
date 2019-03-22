437976d233f1bc7209ff67ad2e15afed226c1a62
Frady <immafrady@gmail.com>
2019-03-22 11:51:37 +0800

    迭代器版本

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ReadExcel = require('./lib/ReadExcel/index')
 const WriteWord = require('./lib/WriteWord/index')
 const SaveImage = require('./lib/SaveImage/index')
 const DirectDownload = require('./lib/DirectDownload/index')
+const HandleArray = require('./lib/HandleArray/index')
 
 const outDir = path.resolve(process.cwd(), 'output')
 
@@ -13,7 +14,10 @@ async function main() {
     const filename = await CommandLineOutput.ask('文件名为？(默认：导入.xlsx)')
     const excelJson = await ReadExcel.readExcel(filename)
     // ------------------------ 导出到文件夹
-    await DirectDownload(excelJson, outDir)
+    const job = HandleArray(excelJson)
+    job.forEach(item => {
+        DirectDownload(item, outDir)
+    })
     console.log('Mission Completed!')
     // ------------------------ 导出到word
     // const myDoc = await WriteWord(excelJson, outDir)
